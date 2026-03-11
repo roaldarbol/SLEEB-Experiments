aefa9f13fb6dd19cc1e4820f9b31aae3c4014001
roaldarbol <25629697+roaldarbol@users.noreply.github.com>
2026-03-11 12:37:58 +0100

    Make scripts directory

--- experiments/scripts/start.py
+++ experiments/scripts/start.py
@@ -1,6 +1,6 @@
 import sys
 import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if script_dir not in sys.path:
     sys.path.insert(0, script_dir)
 
